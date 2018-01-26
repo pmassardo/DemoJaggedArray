f40af44db3b3bc56eee9216b140f8f96c695fd7a
Paul Massardo <paul.massardo@gmail.com>
2018-01-25 21:29:23 -0500

    Add project files.

---
+++ DemoJaggedArray/DemoJaggedArray.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDemoJaggedArray
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.lbOutput = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.cboGenres = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(29, 166)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(66, 23)
+        Me.btnReset.TabIndex = 6
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'lbOutput
+        '
+        Me.lbOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbOutput.Location = New System.Drawing.Point(6, 31)
+        Me.lbOutput.Name = "lbOutput"
+        Me.lbOutput.Size = New System.Drawing.Size(213, 132)
+        Me.lbOutput.TabIndex = 4
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(125, 166)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(66, 23)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'cboGenres
+        '
+        Me.cboGenres.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboGenres.FormattingEnabled = True
+        Me.cboGenres.Items.AddRange(New Object() {"Classical", "Jazz", "Hip Hop", "Rock"})
+        Me.cboGenres.Location = New System.Drawing.Point(78, 5)
+        Me.cboGenres.Name = "cboGenres"
+        Me.cboGenres.Size = New System.Drawing.Size(141, 21)
+        Me.cboGenres.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(3, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Song Genre:"
+        '
+        'frmDemoJaggedArray
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(222, 191)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cboGenres)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.lbOutput)
+        Me.Controls.Add(Me.btnExit)
+        Me.Name = "frmDemoJaggedArray"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Jagged Arrays"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnReset As Button
+    Friend WithEvents lbOutput As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents cboGenres As ComboBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ DemoJaggedArray/DemoJaggedArray.vb
@@ -0,0 +1,101 @@
+﻿''' <summary>
+''' Author:         Alfred Massardo
+''' Project Name:   DemoJaggedArray
+''' Date:           23-Jan-2018
+''' Description:    Simple program to demonstrate creating, loading, looping, and display the content of a jagged array.
+''' </summary>
+
+Public Class frmDemoJaggedArray
+
+    ' constants
+    Private Const indexClassical As Integer = 0     ' Holds the index if the Classical array
+    Private Const indexJazz As Integer = 1          ' Holds the index if the Jazz array
+    Private Const indexHiphop As Integer = 2        ' Holds the index if the Hip Hop array
+    Private Const indexRock As Integer = 3          ' Holds the index if the Rock array
+
+    ' declare the jagged array
+    Private genres As String()() = New String(3)() {} ' Holds the differnt genres of music
+
+    ' Jaggaed arrays can hold arrays with varying numbers of dimensions
+    ' In other words, the child arrays can have varying sizes.
+    Private classical As String() = {"Fugue in G minor", "The Four Seasons", "Adagio for Strings", "Symphony No. 5 In C Minor"} ' Holds 4 Classical songs
+    Private jazz As String() = {"Summertime", "What a Wonderful World", "Feeling Good"} ' Holds 3 Jazz songs
+    Private hiphop As String() = {"Jump Around", "Finesse"} ' Holds 2 Hip Hop songs
+    Private rock As String() = {"Heathens", "We Rock", "Not Falling", "Closer to The Heart"} ' Holds 4 Rock Songs songs
+
+
+    ''' <summary>
+    ''' frmDemoJaggedArray_Load - loads the jagged arrays with the child arrays
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub frmDemoJaggedArray_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        ' load the child arrays
+        genres(indexClassical) = classical  ' load Classical array
+        genres(indexJazz) = jazz            ' load Jazz array
+        genres(indexHiphop) = hiphop        ' load Hip Hop array
+        genres(indexRock) = rock            ' load Rock array
+
+    End Sub
+
+    ''' <summary>
+    ''' cboGenres_SelectedIndexChanged - When the index changes, the index is used to select the child array
+    '''                                  and then loops the elements in the child array
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub cboGenres_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboGenres.SelectedIndexChanged
+
+        ' clear the label text
+        lbOutput.Text = String.Empty
+
+        ' get the index the user selected from the combobox/dropdownlist
+        ' the selected index will be used to select the genre array
+        Dim selectedIndex As Integer = cboGenres.SelectedIndex
+
+        ' Ensure something has been selected by checking that the
+        ' selected index is equal to or greater than zero (0)
+        If selectedIndex >= 0 Then
+
+            ' With the child array selected, using the selected index,
+            ' loop the child array elements
+            For index As Integer = 0 To genres(selectedIndex).Length - 1
+
+                ' Concatinate the content of the child array
+                ' element with a carriage return line feed
+                ' so the content of every element appears on
+                ' a new line
+                lbOutput.Text += genres(selectedIndex)(index) & vbCrLf
+
+            Next index ' next array index
+
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' btnReset_Click - event that resets the form
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        cboGenres.SelectedIndex = -1
+        lbOutput.Text = String.Empty
+
+    End Sub
+
+    ''' <summary>
+    ''' btnExit_Click - Will close the form
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        ' Close the form
+        Me.Close()
+
+    End Sub
+
+End Class
